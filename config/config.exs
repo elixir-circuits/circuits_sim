16d6d25a89486b30a0bdbfdc4be33dd1c813aa1b
Frank Hunleth <fhunleth@troodon-software.com>
2023-04-06 14:57:47 -0700

    Add a TM1620 to the config for development

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
-# Simulate a couple I2C buses with devices on each
+# Simulate a few devices
 config :circuits_sim,
   config: %{
     "i2c-0" => %{0x20 => CircuitsSim.Device.MCP23008, 0x50 => CircuitsSim.Device.AT24C02},
@@ -8,8 +8,10 @@ config :circuits_sim,
       0x10 => CircuitsSim.Device.ADS7138,
       0x20 => CircuitsSim.Device.MCP23008,
       0x21 => CircuitsSim.Device.MCP23008
-    }
+    },
+    "spidev0.0" => %{0 => {CircuitsSim.Device.TM1620, render: :binary_clock}}
   }
 
-# Tell Circuits.I2C to default to simulated I2C buses
+# Use the simulated I2C and SPI buses
 config :circuits_i2c, default_backend: CircuitsSim.I2C.Backend
+config :circuits_spi, default_backend: CircuitsSim.SPI.Backend
