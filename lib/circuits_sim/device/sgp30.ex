bd1f1aebb575855a13231141639ebf70146fef4c
Masatoshi N <nishiguchi.masa@gmail.com>
2023-04-21 08:00:47 -0400

    use handle_message

--- lib/circuits_sim/device/sgp30.ex
+++ lib/circuits_sim/device/sgp30.ex
@@ -5,12 +5,26 @@ defmodule CircuitsSim.Device.SGP30 do
   Typically found at 0x58
   See the [datasheet](https://www.mouser.com/datasheet/2/682/Sensirion_Gas_Sensors_SGP30_Datasheet_EN-1148053.pdf)
   Many features aren't implemented.
+
+  Call the following functions to change the state of the sensor.
+
+  * `set_tvoc_ppb/3`
+  * `set_co2_eq_ppm/3`
+  * `set_h2_raw/3`
+  * `set_ethanol_raw/3`
   """
   alias CircuitsSim.I2C.I2CDevice
   alias CircuitsSim.I2C.I2CServer
 
-  defstruct current: nil
-  @type t() :: %__MODULE__{current: atom()}
+  defstruct current: nil, tvoc_ppb: 0, co2_eq_ppm: 0, h2_raw: 0, ethanol_raw: 0
+
+  @type t() :: %__MODULE__{
+          current: atom(),
+          tvoc_ppb: integer(),
+          co2_eq_ppm: integer(),
+          h2_raw: integer(),
+          ethanol_raw: integer()
+        }
 
   @spec child_spec(keyword()) :: Supervisor.child_spec()
   def child_spec(args) do
@@ -18,41 +32,120 @@ defmodule CircuitsSim.Device.SGP30 do
     I2CServer.child_spec_helper(device, args)
   end
 
-  @spec new() :: %__MODULE__{current: nil}
+  @spec new() :: %__MODULE__{
+          current: nil,
+          tvoc_ppb: 0,
+          co2_eq_ppm: 0,
+          h2_raw: 0,
+          ethanol_raw: 0
+        }
   def new() do
     %__MODULE__{}
+  end
+
+  @spec set_tvoc_ppb(String.t(), Circuits.I2C.address(), integer()) :: :ok
+  def set_tvoc_ppb(bus_name, address, value) when is_number(value) do
+    I2CServer.send_message(bus_name, address, {:set_tvoc_ppb, value})
+  end
+
+  @spec set_co2_eq_ppm(String.t(), Circuits.I2C.address(), integer()) :: :ok
+  def set_co2_eq_ppm(bus_name, address, value) when is_number(value) do
+    I2CServer.send_message(bus_name, address, {:set_co2_eq_ppm, value})
+  end
+
+  @spec set_h2_raw(String.t(), Circuits.I2C.address(), integer()) :: :ok
+  def set_h2_raw(bus_name, address, value) when is_number(value) do
+    I2CServer.send_message(bus_name, address, {:set_h2_raw, value})
+  end
+
+  @spec set_ethanol_raw(String.t(), Circuits.I2C.address(), integer()) :: :ok
+  def set_ethanol_raw(bus_name, address, value) when is_number(value) do
+    I2CServer.send_message(bus_name, address, {:set_ethanol_raw, value})
   end
 
   ## protocol implementation
 
   defimpl I2CDevice do
     @impl I2CDevice
-    def read(%{current: :iaq_measure} = state, 6) do
-      {<<1, 144, 76, 0, 0, 129>>, %{state | current: nil}}
+    def read(%{current: :iaq_measure} = state, count) do
+      result = binary_for_measure(state) |> trim_pad(count)
+      {result, %{state | current: nil}}
     end
 
-    def read(%{current: :iaq_measure_raw} = state, 6) do
-      {<<53, 32, 194, 73, 160, 55>>, %{state | current: nil}}
+    def read(%{current: :iaq_measure_raw} = state, count) do
+      result = binary_for_measure_raw(state) |> trim_pad(count)
+      {result, %{state | current: nil}}
+    end
+
+    def read(state, count) do
+      {:binary.copy(<<0>>, count), %{state | current: nil}}
     end
 
     @impl I2CDevice
     def write(state, <<0x20, 0x03>>), do: %{state | current: :iaq_init}
     def write(state, <<0x20, 0x08>>), do: %{state | current: :iaq_measure}
     def write(state, <<0x20, 0x50>>), do: %{state | current: :iaq_measure_raw}
+    def write(state, _), do: state
 
     @impl I2CDevice
-    def write_read(state, <<0x36, 0x82>>, 9) do
-      {<<0, 0, 129, 1, 142, 16, 41, 222, 133>>, %{state | current: nil}}
+    def write_read(state, <<0x36, 0x82>>, count) do
+      result = binary_for_serial(state) |> trim_pad(count)
+      {result, %{state | current: nil}}
     end
+
+    def write_read(state, _to_write, read_count) do
+      {:binary.copy(<<0>>, read_count), %{state | current: nil}}
+    end
+
+    defp trim_pad(x, count) when byte_size(x) >= count, do: :binary.part(x, 0, count)
+    defp trim_pad(x, count), do: x <> :binary.copy(<<0>>, count - byte_size(x))
 
     @impl I2CDevice
     def render(state) do
-      "current: #{state.current}\n"
+      [
+        "tvoc_ppb: #{state.tvoc_ppb}",
+        "co2_eq_ppm: #{state.co2_eq_ppm}",
+        "h2_raw: #{state.h2_raw}",
+        "ethanol_raw: #{state.ethanol_raw}"
+      ]
+      |> Enum.join(", ")
     end
 
     @impl I2CDevice
-    def handle_message(state, _message) do
-      {:not_implemented, state}
+    def handle_message(state, {:set_tvoc_ppb, value}) do
+      {:ok, %{state | tvoc_ppb: value}}
+    end
+
+    def handle_message(state, {:set_co2_eq_ppm, value}) do
+      {:ok, %{state | co2_eq_ppm: value}}
+    end
+
+    def handle_message(state, {:set_h2_raw, value}) do
+      {:ok, %{state | h2_raw: value}}
+    end
+
+    def handle_message(state, {:set_ethanol_raw, value}) do
+      {:ok, %{state | ethanol_raw: value}}
+    end
+
+    defp binary_for_serial(_state) do
+      <<0, 0, 129, 1, 142, 16, 41, 222, 133>>
+    end
+
+    defp binary_for_measure(state) do
+      co2_eq_ppm = state.co2_eq_ppm
+      tvoc_ppb = state.tvoc_ppb
+      crc1 = SGP30.CRC.calculate(co2_eq_ppm)
+      crc2 = SGP30.CRC.calculate(tvoc_ppb)
+      <<co2_eq_ppm::16, crc1, tvoc_ppb::16, crc2>>
+    end
+
+    defp binary_for_measure_raw(state) do
+      h2_raw = state.h2_raw
+      ethanol_raw = state.ethanol_raw
+      crc1 = SGP30.CRC.calculate(h2_raw)
+      crc2 = SGP30.CRC.calculate(ethanol_raw)
+      <<h2_raw::16, crc1, ethanol_raw::16, crc2>>
     end
   end
 end
--- test/circuits_sim/device/sgp30_test.exs
+++ test/circuits_sim/device/sgp30_test.exs
@@ -1,8 +1,29 @@
 defmodule CircuitsSim.Device.SGP30Test do
   use ExUnit.Case
 
-  test "measurement" do
+  alias CircuitsSim.I2C.I2CServer
+
+  test "simple usage" do
+    device = CircuitsSim.Device.SGP30.new()
+    init_arg = [bus_name: "i2c-1", address: 0x58, device: device]
+    I2CServer.start_link(init_arg)
+
+    I2CServer.send_message("i2c-1", 0x58, {:set_tvoc_ppb, 0})
+    I2CServer.send_message("i2c-1", 0x58, {:set_co2_eq_ppm, 400})
+    I2CServer.send_message("i2c-1", 0x58, {:set_h2_raw, 13600})
+    I2CServer.send_message("i2c-1", 0x58, {:set_ethanol_raw, 18848})
+
+    assert I2CServer.render("i2c-1", 0x58) ==
+             "tvoc_ppb: 0, co2_eq_ppm: 400, h2_raw: 13600, ethanol_raw: 18848"
+  end
+
+  test "supports SGP30 package" do
     {:ok, sgp_pid} = SGP30.start_link(bus_name: "i2c-1")
+
+    I2CServer.send_message("i2c-1", 0x58, {:set_tvoc_ppb, 0})
+    I2CServer.send_message("i2c-1", 0x58, {:set_co2_eq_ppm, 400})
+    I2CServer.send_message("i2c-1", 0x58, {:set_h2_raw, 13600})
+    I2CServer.send_message("i2c-1", 0x58, {:set_ethanol_raw, 18848})
 
     # SGP30 requires measurements at specific intervals, so it may take 900ms on startup
     # before the first one. Since this is a mock device and we don't want
