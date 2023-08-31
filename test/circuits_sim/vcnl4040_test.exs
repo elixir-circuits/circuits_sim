c88bb7bb28199a3e8f9d15ae5f92c3a304f05873
Eric Oestrich <eric@oestrich.org>
2023-08-31 15:48:39 -0400

    Add a device sim for the VCNL4040 proximity sensor
    - Setting config and logging what changed
    - Set the proximity/ambient light/white light and fetch from the data
      registers
    - Return the chip id
    
    Datasheet link https://www.vishay.com/docs/84274/vcnl4040.pdf

---
+++ test/circuits_sim/vcnl4040_test.exs
@@ -0,0 +1,37 @@
+defmodule CircuitsSim.Device.VCNL4040Test do
+  use ExUnit.Case
+
+  alias CircuitsSim.I2C.I2CServer
+  alias CircuitsSim.Device.VCNL4040, as: VCNL4040Sim
+
+  @i2c_address 0x60
+  @ps_data 0x08
+  @als_data 0x09
+  @wl_data 0x0A
+
+  test "rendering the sensor", %{test: test_name} do
+    i2c_bus = to_string(test_name)
+    start_supervised!({VCNL4040Sim, bus_name: i2c_bus, address: @i2c_address})
+
+    rendered = I2CServer.render(i2c_bus, @i2c_address)
+
+    assert rendered ==
+             "Ambient light sensor output\n\nProximity: 0\nAmbient Light: 0\nWhite Light: 0\n"
+  end
+
+  test "supports VCNL4040 package", %{test: test_name} do
+    i2c_bus = to_string(test_name)
+    start_supervised!({VCNL4040Sim, bus_name: i2c_bus, address: @i2c_address})
+
+    VCNL4040Sim.set_proximity(i2c_bus, @i2c_address, 10)
+    VCNL4040Sim.set_ambient_light(i2c_bus, @i2c_address, 50)
+    VCNL4040Sim.set_white_light(i2c_bus, @i2c_address, 40)
+
+    assert {:ok, <<10::little-16>>} = I2CServer.write_read(i2c_bus, @i2c_address, <<@ps_data>>, 2)
+
+    assert {:ok, <<50::little-16>>} =
+             I2CServer.write_read(i2c_bus, @i2c_address, <<@als_data>>, 2)
+
+    assert {:ok, <<40::little-16>>} = I2CServer.write_read(i2c_bus, @i2c_address, <<@wl_data>>, 2)
+  end
+end
