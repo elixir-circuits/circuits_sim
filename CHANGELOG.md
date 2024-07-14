3c2825968a0b46791d6f6e7ae7bdefaddfa23196
Frank Hunleth <fhunleth@troodon-software.com>
2024-07-14 12:14:37 -0400

    v0.1.1 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v0.1.1
+
+* Changes
+  * Fix warning when creating input GPIOs. (@pojiro)
+  * Fix Elixir 1.17 warnings
+
 ## v0.1.0
 
 Initial release
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CircuitsSim.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "Simulated hardware for Elixir Circuits"
   @source_url "https://github.com/elixir-circuits/circuits_sim"
 
